a3f77cbadfd27d50a9ce8ab94ad42fd4e4ab95dc
INNA <innabogd@mail.ru>
2016-10-15 15:32:04 +0700

    Задача Range

---
+++ .idea/src/ru/academit/homeworks/range/Main.java
@@ -0,0 +1,12 @@
+package ru.academit.homeworks.range;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Range userParameters = new Range(3.099899, 3.099999, 8.8);
+
+        userParameters.isInside();
+
+    }
+}
