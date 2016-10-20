6f5fb4ea956db22f32e54f06695d211a69f7badf
INNA <innabogd@mail.ru>
2016-10-20 23:05:48 +0700

    Третий блок исправлений

--- Range/src/ru/academit/homeworks/range01/Main.java
+++ Range/src/ru/academit/homeworks/range01/Main.java
@@ -4,6 +4,22 @@ import java.util.Scanner;
 
 public class Main {
 
+    private Range firstInterval;
+    private Range secondInterval;
+
+    public Main(double from1, double from2, double to1, double to2) {
+        firstInterval = new Range(from1, to1);
+        secondInterval = new Range(from2, to2);
+    }
+
+    public Range getFirstInterval() {
+        return firstInterval;
+    }
+
+    public Range getSecondInterval() {
+        return secondInterval;
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Введите число:");
@@ -12,8 +28,15 @@ public class Main {
         double userNumber = scanner.nextDouble();
 
         Range userParameters = new Range(3.099999, 8.8);
+        Range userFirstInterval = new Range(3, 7);
+        Range userSecondInterval = new Range(2, 6);
 
         userParameters.isInside(userNumber);
 
+        userFirstInterval.getCrossInterval(userSecondInterval);
+        userFirstInterval.getGeneralInterval(userSecondInterval);
+        userFirstInterval.getSubtractionInterval(userSecondInterval);
+
     }
 }
+
--- Range/src/ru/academit/homeworks/range01/Range.java
+++ Range/src/ru/academit/homeworks/range01/Range.java
@@ -12,12 +12,125 @@ public class Range {
         this.to = to;
     }
 
+    public double getFrom() {
+        return from;
+    }
+    public void setFrom(double from) {
+        this.from = from;
+    }
+    public double getTo() {
+        return to;
+    }
+    public void setTo(double to) {
+        this.to = to;
+    }
+
     public boolean isInside(double number) {
 
-        if (((Math.abs(number - from) <= EPSILON)||(number > from)) && ((Math.abs(number - to) <= EPSILON)||(number < to))) {
+        if (((Math.abs(number - from) <= EPSILON) || (number > from)) && ((Math.abs(number - to) <= EPSILON) || (number < to))) {
             return true;
         } else {
             return false;
         }
     }
+
+    public double calcDistance(Range range) {
+        return to - from;
+    }
+
+    public void getCrossInterval(Range range) {
+        String s = "";
+        String fromString = "";
+        String toString = "";
+        if (to < range.from) {
+            System.out.println("Нет пересечения интервалов");;
+        } else {
+            if ((range.to < to) && (from < range.from)) {
+                fromString = Double.toString(range.from);
+                toString = Double.toString(range.to);
+                System.out.println(fromString + " " + toString);
+            } else if ((range.to > to) && (from < range.from)) {
+                fromString = Double.toString(range.from);
+                toString = Double.toString(to);
+                System.out.println(fromString + " " + toString);
+            } else if ((range.to < to) && (from > range.from)) {
+                fromString = Double.toString(from);
+                toString = Double.toString(range.to);
+                System.out.println(fromString + " " + toString);
+            } else {
+                fromString = Double.toString(from);
+                toString = Double.toString(to);
+                System.out.println(fromString + " " + toString);
+            }
+        }
+    }
+
+    public void getGeneralInterval(Range range) {
+        String s = "";
+        String fromString = "";
+        String toString = "";
+        if (to < range.from) {
+            System.out.println("Общий интервал: от" + from + "до" + to + "и от" + range.from + "до" + range.to);;
+        } else {
+            if ((range.to < to) && (from < range.from)) {
+                fromString = Double.toString(from);
+                toString = Double.toString(to);
+                System.out.println(fromString + " " + toString);
+            } else if ((range.to > to) && (from < range.from)) {
+                fromString = Double.toString(from);
+                toString = Double.toString(range.to);
+                System.out.println(fromString + " " + toString);
+            } else if ((range.to < to) && (from > range.from)) {
+                fromString = Double.toString(range.from);
+                toString = Double.toString(to);
+                System.out.println(fromString + " " + toString);
+            } else {
+                fromString = Double.toString(range.from);
+                toString = Double.toString(range.to);
+                System.out.println(fromString + " " + toString);
+            }
+        }
+    }
+
+    public void getSubtractionInterval(Range range) {
+        String s = "";
+        String fromString = "";
+        String toString = "";
+        if (to < range.from) {
+            System.out.println("от " + from + " до " + to + " и от " + range.from + " до " + range.to);;
+        } else {
+            if ((range.to < to) && (from < range.from)) {
+                fromString = "от " + Double.toString(from) + " до" + Double.toString(range.from);
+                toString = "от " + Double.toString(range.to) + " до" + Double.toString(range.to);
+                System.out.println(fromString + "и" + toString);
+            } else if ((range.to > to) && (from < range.from)) {
+                fromString = "от " + Double.toString(from) + " до" + Double.toString(range.from);
+                toString = "от " + Double.toString(to) + " до" + Double.toString(range.to);
+                System.out.println(fromString + "и" + toString);
+            } else if ((range.to < to) && (from > range.from)) {
+                fromString = "от " + Double.toString(range.from) + " до" + Double.toString(from);
+                toString = "от " + Double.toString(range.to) + " до" + Double.toString(to);
+                System.out.println(fromString + "и" + toString);
+            } else if ((Math.abs(range.to - to) <= EPSILON) && (from > range.from)) {
+                fromString = "от " + Double.toString(range.from) + " до" + Double.toString(from);
+                System.out.println(fromString);
+            } else if ((Math.abs(range.to - to) <= EPSILON) && (range.from > from)) {
+                fromString = "от " + Double.toString(from) + " до" + Double.toString(range.from);
+                System.out.println(fromString);
+            } else if ((Math.abs(range.from - from) <= EPSILON) && (range.to > to)) {
+                fromString = "от " + Double.toString(to) + " до" + Double.toString(range.to);
+                System.out.println(fromString);
+            } else if ((Math.abs(range.from - from) <= EPSILON) && (to > range.to)) {
+                fromString = "от " + Double.toString(range.from) + " до" + Double.toString(from);
+                System.out.println(fromString);
+            } else {
+                fromString = "от " + Double.toString(range.from) + " до" + Double.toString(from);
+                toString = "от " + Double.toString(to) + " до" + Double.toString(range.to);
+                System.out.println(fromString + "и" + toString);
+            }
+        }
+    }
+
+
 }
+
